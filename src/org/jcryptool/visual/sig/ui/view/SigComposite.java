75c888ef2d1d2cdbcb93d60d2d25736c1055fbe2
Florentina Grebe <florentina.grebe@gmx.net>
2013-04-10 10:57:15 +0200

    Some GUI changes

--- src/org/jcryptool/visual/sig/ui/view/SigComposite.java
+++ src/org/jcryptool/visual/sig/ui/view/SigComposite.java
@@ -6,9 +6,13 @@ import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Text;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Group;
 
 public class SigComposite extends Composite implements PaintListener{
 	private Text txtHash;
+	private Text txtGeneralDescription;
+	private Text txtStepDescription;
+	private Text txtSignature;
 
 	public SigComposite(Composite parent, int style, SigView view) {
 		super(parent, style);
@@ -21,6 +25,28 @@ public class SigComposite extends Composite implements PaintListener{
 		Button btnHash = new Button(this, SWT.NONE);
 		btnHash.setBounds(28, 293, 136, 62);
 		btnHash.setText("Hashfunktion");
+		
+		Button btnSignature = new Button(this, SWT.NONE);
+		btnSignature.setBounds(299, 413, 122, 56);
+		btnSignature.setText("Signaturfunktion");
+		
+		txtGeneralDescription = new Text(this, SWT.BORDER);
+		txtGeneralDescription.setEditable(false);
+		txtGeneralDescription.setBounds(10, 10, 666, 45);
+		
+		txtStepDescription = new Text(this, SWT.BORDER | SWT.V_SCROLL | SWT.MULTI);
+		txtStepDescription.setBounds(204, 118, 271, 147);
+		
+		Group grpDokumentAnEmpfnger = new Group(this, SWT.NONE);
+		grpDokumentAnEmpfnger.setText("Dokument an Empf\u00E4nger");
+		grpDokumentAnEmpfnger.setBounds(441, 288, 212, 203);
+		
+		txtSignature = new Text(grpDokumentAnEmpfnger, SWT.BORDER);
+		txtSignature.setBounds(10, 110, 178, 56);
+		
+		Button btnReset = new Button(this, SWT.NONE);
+		btnReset.setBounds(582, 497, 94, 28);
+		btnReset.setText("Reset");
 		// TODO Auto-generated constructor stub
 	}
 
